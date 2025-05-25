d45fbe3401f8d4ef4e499ba0986129fee881af64
emreyilmaz <emreyilmazxy@gmail.com>
2025-05-25 15:52:18 +0300

    Add project files.

---
+++ Soyutlar/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soyutlar
+{
+    public abstract class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Department { get; set; }
+
+
+        public abstract void Work();
+
+
+
+    }
+
+    public class SoftwareDeveloper : Person
+    {
+        public override void Work()
+        {
+            Console.WriteLine($"{Name} {Surname}. şirketin {Department} departmanında Yazılım Geliştirici olarak çalışıyorum.");
+        }
+    }
+    public class SoftwareTester : Person
+    {
+        public override void Work()
+        {
+            Console.WriteLine($"{Name} {Surname}. şirketin {Department} departmanında QA Engineer olarak çalışıyorum.");
+        }
+    }
+    public class ScrumMaster : Person
+    {
+        public override void Work()
+        {
+            Console.WriteLine($"{Name} {Surname}.  şirketin {Department} departmanında takım kordinatörü olarak çalışıyorum.");
+        }
+    }
+}
+
---
+++ Soyutlar/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Soyutlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            SoftwareDeveloper developer = new SoftwareDeveloper
+            {
+                Name = "Emre",
+                Surname = "Yıldız",
+                Department = "Yazılım Geliştirme"
+            };
+
+            
+            SoftwareTester tester = new SoftwareTester
+            {
+                Name = "Zeynep",
+                Surname = "Koç",
+                Department = "Yazılım Geliştirme"
+            };
+
+          
+            ScrumMaster scrumMaster = new ScrumMaster
+            {
+                Name = "Hasan",
+                Surname = "Çıldırmış",
+                Department = "Yazılım Geliştirme"
+            };
+
+          
+            developer.Work();
+            tester.Work();
+            scrumMaster.Work();
+
+           
+        }
+    }
+}
